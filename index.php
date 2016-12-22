dafaf9bcccf60ee8399e87621031c797dc269a53
Shibin <442171804@qq.com>
2016-12-22 21:14:06 +0800

    ajax login

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="UTF-8">
+	<title>Test Ajax</title>
+</head>
+<body>
+	<div class="login-form">
+		<form action="#">
+			<label for="username">Username <input id="username" name="username" type="text"></label>
+			<label for="password">Password <input id="password" name="password" type="password"></label>
+			<button type="button" id="login-btn">Login</button>
+		</form>
+	</div>
+	<script src="http://apps.bdimg.com/libs/jquery/1.9.0/jquery.min.js"></script>
+	<script>
+		$(function() {
+			// username: admin
+			// password: admin
+			var requestUrl = 'login.php';
+			var username = $('input[name="username"]');
+			var password = $('input[name="password"]');
+			
+			$('button#login-btn').click(function()  {
+				$.post(requestUrl, {username: username.val(), password: password.val()}, function(result) {
+					if(result.status) {
+						alert(result.message);
+						window.location.href="hsttp://www.baidu.com";
+					} else {
+						alert(result.message);
+						return false;
+					}
+				}, 'JSON');
+			});
+			
+
+		});
+	</script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if($username == 'admin' && $password == 'admin') {
+	//用户名和密码都正确
+	$array = array('status' => 1, 'message' => '登录成功');
+	$result = json_encode($array);
+	echo $result;
+} else {
+	//用户名和密码不正确
+	$array = array('status' => 0, 'message' => '登录失败');
+	$result = json_encode($array);
+	echo $result;
+}
+exit();
